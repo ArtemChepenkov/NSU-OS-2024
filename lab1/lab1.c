ae73783b99ff4eccf8f5afe0b1f69d12f3893024
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-09-14 13:31:00 +0700

    fixed

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,115 +1,72 @@
-﻿#include <stdio.h>
+﻿#include <sys/resource.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <unistd.h>
-
-#include <assert.h>
 #include <ulimit.h>
-#include <sys/resource.h>
+
 
 int main(int argc, char* argv[]) {
-    int c;
-    extern char* optarg;
-    extern int optind, optopt;
+	int opt;
+	struct rlimit core_f;
+	extern char** environ;
+	char** env;
+	while ((opt = getopt(argc, argv, "ispuU:cC:dvV:")) != -1) {
+		switch (opt) {
+		case('i'):
+			printf("Real userid = %d\n", getuid());
+			printf("Effective userid = %d\n", geteuid());
+			printf("Real groupid = %d\n", getgid());
+			printf("Effective groupid = %d\n", getegid());
+			break;
 
-    extern char** environ;
+		case('s'):
+			setpgid(0, 0);
+			break;
 
-    pid_t pid, pgid, ppid;
-    uid_t euid, ruid;
-    gid_t egid, rgid;
-    long ulim;
-    char* cwd;
-    struct rlimit core_lim;
+		case('p'):
+			printf("Process number = %d\n", getpid());
+			printf("Parent process number = %d\n", getppid());
+			printf("Group process number = %d\n", getpgid(0));
+			break;
 
-    while ((c = getopt(argc, argv, "U:uispdvV:cC:")) != -1) {
-        switch (c) {
-        case 'i':
-            euid = geteuid();
-            ruid = getuid();
-            egid = getegid();
-            rgid = getgid();
-            printf(
-                "Effective user id = %d\nReal user id = %d\nEffective group id = %d\nReal group id = %d\n",
-                euid, ruid, egid, rgid
-            );
-            break;
-        case 's':
-            pid = getpid();
-            if (setpgid(pid, pid) == -1) {
-                perror("Failed to set group id");
-            }
-            break;
-        case 'p':
-            pid = getpid();
-            pgid = getpgrp();
-            ppid = getppid();
-            printf(
-                "Process id = %d\nParent process id = %d\nProcess group id = %d\n",
-                pid,
-                ppid,
-                pgid
-            );
-            break;
-        case 'u':
-            ulim = ulimit(UL_GETFSIZE);
-            if (ulim == -1) {
-                perror("Failed to get ulimit");
-                continue;
-            }
-            printf("ulimit = %ld\n", ulim * 512);
-            break;
-        case 'U':
-            ulim = atol(optarg);
-            if (ulim % 512 != 0) {
-                fprintf(stderr, "New ulimit value is not divisible by 512\n");
-                continue;
-            }
-            ulim = ulimit(UL_SETFSIZE, ulim / 512);
-            if (ulim == -1) {
-                perror("Failed to set ulimit");
-                continue;
-            }
-            break;
-        case 'd':
-            cwd = getcwd(NULL, 256);
-            if (cwd == NULL) {
-                perror("Failed to get cwd");
-                continue;
-            }
-            printf("Current working directory: %s\n", cwd);
-            free(cwd);
-            cwd = NULL;
-            break;
-        case 'v':
-            for (int i = 0; environ[i]; i++) {
-                printf("%s\n", environ[i]);
-            }
-            break;
-        case 'V':
-            if (putenv(optarg) != 0) {
-                perror("Failed to put env variable");
-            }
-            break;
-        case 'c':
-            if (getrlimit(RLIMIT_CORE, &core_lim) != 0)
-                perror("Failed to get core limit");
-            printf("Hard core limit = %lu\nSoft core limit = %lu\n", core_lim.rlim_max, core_lim.rlim_cur);
-            break;
-        case 'C':
-            if (getrlimit(RLIMIT_CORE, &core_lim) != 0)
-                perror("Failed to get core limit");
-            core_lim.rlim_cur = atol(optarg);
-            if (setrlimit(RLIMIT_CORE, &core_lim) != 0)
-                perror("Failed to set core limit");
-            break;
-        case ':':
-            fprintf(stderr, "Option -%c requires an operand\n", optopt);
-            break;
-        case '?':
-            fprintf(stderr, "Unrecognized option: -%c\n", optopt);
-            break;
-        }
-    }
+		case('u'):
+			printf("ulimit = %ld\n", ulimit(UL_GETFSIZE) * 512);
+			break;
 
-    exit(0);
+		case('U'):
+			long int new_ulimit = atol(optarg);
+			new_ulimit = ulimit(UL_SETFSIZE, new_ulimit / 512);
+			break;
+
+		case('c'):
+			getrlimit(RLIMIT_CORE, &core_f);
+			printf("core size = %ld\n", core_f.rlim_cur);
+			break;
+
+		case('C'):
+			//check
+			getrlimit(RLIMIT_CORE, &core_f);
+			core_f.rlim_cur = atol(optarg);
+			if (setrlimit(RLIMIT_CORE, &core_f) == -1)
+				fprintf(stderr, "Must be super-user to increase core\n");
+			break;
+
+		case('d'):
+			printf("current working directory is: %s\n",
+				getcwd(NULL, 100));
+			break;
+
+		case('v'):
+			env = environ;
+			while (*env) {
+				printf("%s\n", *env);
+				env++;
+			}
+			break;
+
+		case('V'):
+			putenv(optarg);
+			break;
+		}
+	}
 }
