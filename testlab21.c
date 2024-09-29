63258837b355e587216fd5e5a5d9120a94429977
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-09-29 19:49:44 +0700

    added testlab21

--- testlab21.c
+++ testlab21.c
@@ -45,7 +45,15 @@ int main() {
     sigint_action.sa_handler = handleSigint;
     sigquit_action.sa_handler = handleSigquit;
 
-    sigaction(SIGINT, &sigint_action, NULL);
+    if (sigaction(SIGINT, &sigint_action, NULL) == -1) {
+	perror("Failed to set SIGINT handler");
+	exit(EXIT_FAILURE);
+    }
+
+    if (sigaction(SIGQUIT, &sigquit_action, NULL) == -1) {
+	perror("Failed to set SIGQUIT handler");
+	exit(EXIT_FAILURE);
+    }
 
     while (1) {
         pause();
