01c94a58ba1b9cea84f216eb3d50038c84c20a06
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-09-30 16:33:51 +0700

    added changes

--- testlab21.c
+++ testlab21.c
@@ -13,29 +13,18 @@ void handleSigint() {
 void handleSigquit() {
     char beepAmount[10];
     int index = 0;
-    if (counter == 0) {
-        beepAmount[index++] = '0';
-    }
-    while (counter > 0) {
-        beepAmount[index++] = (counter % 10) + '0';
-        counter /= 10;
-    }
-    index--;
 
     if (write(1, "\nAmount of beeps: ", 18) == -1) {
         _exit(EXIT_FAILURE);
     }
 
-    for (int i = index; i >= 0; i--) {
-        if (write(1, &beepAmount[i], 1) == -1) {
-            _exit(EXIT_FAILURE);
-        }
+    if (write(1, &counter, 4) == -1) {
+        _exit(EXIT_FAILURE);
     }
 
     if (write(1, "\n", 1) == -1) {
         _exit(EXIT_FAILURE);
     }
-
     _exit(EXIT_SUCCESS);
 }
 
@@ -46,13 +35,13 @@ int main() {
     sigquit_action.sa_handler = handleSigquit;
 
     if (sigaction(SIGINT, &sigint_action, NULL) == -1) {
-	perror("Failed to set SIGINT handler");
-	exit(EXIT_FAILURE);
+        perror("Failed to set SIGINT handler");
+        exit(EXIT_FAILURE);
     }
 
     if (sigaction(SIGQUIT, &sigquit_action, NULL) == -1) {
-	perror("Failed to set SIGQUIT handler");
-	exit(EXIT_FAILURE);
+        perror("Failed to set SIGQUIT handler");
+        exit(EXIT_FAILURE);
     }
 
     while (1) {
